2917a5ab57c5a674af4a0b3e8ef1670379221104
asastg <asastoqueg@unal.edu.co>
2024-06-22 17:41:35 -0500

    arrays, asynchronous & conditional rending

--- examples/counter/src/App.jsx
+++ examples/counter/src/App.jsx
@@ -1,25 +1,39 @@
 import { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>;
-const Button = ({ onClcik, text }) => <button onClick={onClcik}>{text}</button>;
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(' ')}</div>;
+};
+const Button = ({ handleClcik, text }) => <button onClick={handleClcik}>{text}</button>;
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
   const handleLeftClick = () => {
-    setClicks({ ...clicks, left: clicks.left + 1 });
-  };
-  const handleRightClick = () => {
-    setClicks({ ...clicks, right: clicks.right + 1 });
+    setAll(allClicks.concat('L'));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
   };
 
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(updatedRight + left);
+  };
   return (
     <>
-      {clicks.left}
-      <Button onClcik={handleLeftClick} text="left" />
-      <Button onClcik={handleRightClick} text="right" />
-      {clicks.right}
+      {left}
+      <Button handleClcik={handleLeftClick} text="left" />
+      <Button handleClcik={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+      <p>Total {total}</p>
     </>
   );
 };
