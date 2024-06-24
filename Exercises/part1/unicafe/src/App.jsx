4b189454e10aea1980a12a444cbe576771f5751b
asastg <asastoqueg@unal.edu.co>
2024-06-23 20:12:23 -0500

    exercise part1.11

--- Exercises/part1/unicafe/src/App.jsx
+++ Exercises/part1/unicafe/src/App.jsx
@@ -2,28 +2,27 @@ import { useState } from 'react';
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
 
-const StatisticLine = ({ text, value }) => {
-  return (
-    <>
-      <p>
-        {text} {value}
-      </p>
-    </>
-  );
-};
+const StatisticLine = ({ text, value }) => (
+  <tbody>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  </tbody>
+);
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   if (!all) {
     return <>No feedback given</>;
   }
   return (
-    <>
+    <table>
       <StatisticLine text={'good'} value={good} />
       <StatisticLine text={'neutral'} value={neutral} />
       <StatisticLine text={'bad'} value={bad} />
       <StatisticLine text={'all'} value={all} />
       <StatisticLine text={'average'} value={average()} />
       <StatisticLine text={'positive'} value={positive()} />
-    </>
+    </table>
   );
 };
 const App = () => {
@@ -59,7 +58,7 @@ const App = () => {
       <Button handleClick={handleClickGood} text={'good'} />
       <Button handleClick={handleClickNeutral} text={'neutral'} />
       <Button handleClick={handleClickBad} text={'bad'} />
-      <h1>Statistics</h1>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
   );
