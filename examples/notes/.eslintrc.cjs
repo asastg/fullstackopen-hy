005d46d5652077fc53fcb3eba89ab7ce5262fd6c
asastg <asastoqueg@unal.edu.co>
2024-06-25 12:35:33 -0500

    refactoring modules

--- examples/notes/.eslintrc.cjs
+++ examples/notes/.eslintrc.cjs
@@ -9,6 +9,6 @@ module.exports = {
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'react/props-types': 0,
+    'react/prop-types': 0,
   },
 };
