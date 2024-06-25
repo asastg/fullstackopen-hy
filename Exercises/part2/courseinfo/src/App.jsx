31795c4e5b926c76a56a790989bab22b2c627c47
asastg <asastoqueg@unal.edu.co>
2024-06-25 13:59:50 -0500

    exercise part2.5

--- Exercises/part2/courseinfo/src/App.jsx
+++ Exercises/part2/courseinfo/src/App.jsx
@@ -1,43 +1,4 @@
-const Header = ({ name }) => {
-  return (
-    <>
-      <h1>{name}</h1>
-    </>
-  );
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <>
-      <p>
-        {name} {exercises}
-      </p>
-    </>
-  );
-};
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </>
-  );
-};
-
-const Total = ({ parts }) => {
-  return <p>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</p>;
-};
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
-
+import Course from './components/Course';
 const App = () => {
   const courses = [
     {
