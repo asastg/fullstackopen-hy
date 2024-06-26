23727f5daa569a465e0f1e6584eac4aedb1232f4
asastg <asastoqueg@unal.edu.co>
2024-06-26 17:37:51 -0500

    exercise part2.11

--- Exercises/part2/phonebook/src/App.jsx
+++ Exercises/part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 const Person = ({ name, number }) => {
   return (
     <div>
@@ -27,16 +28,16 @@ const Form = ({ addPerson, newName, handleNameChange, number, handleNumberChange
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [number, setNumber] = useState('');
   const [nameFilter, setNameFilter] = useState('');
   const [showAll, setShowAll] = useState(true);
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
   const handleNameFilter = (event) => {
     setShowAll(false);
     setNameFilter(event.target.value);
