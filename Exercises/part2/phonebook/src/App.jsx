71bb901e1dd4935cfd8497e0cf921c40ee22a625
asastg <asastoqueg@unal.edu.co>
2024-06-26 15:13:04 -0500

    exercise part2.9

--- Exercises/part2/phonebook/src/App.jsx
+++ Exercises/part2/phonebook/src/App.jsx
@@ -5,9 +5,23 @@ const Person = ({ name, number }) => (
   </div>
 );
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '39-44-5323523' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
   const [number, setNumber] = useState('');
+  const [nameFilter, setNameFilter] = useState('');
+  const [showAll, setShowAll] = useState(true);
+  const handleNameFilter = (event) => {
+    setShowAll(false);
+    setNameFilter(event.target.value);
+    if (event.target.value === '') {
+      setShowAll(true);
+    }
+  };
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
@@ -42,9 +56,14 @@ const App = () => {
   //   }
   //   setNewName('');
   // };
+
+  const personsToShow = showAll ? persons : persons.filter((person) => person.name === nameFilter);
+
   return (
     <>
       <h2>Phonebook</h2>
+      filter shown with <input onChange={handleNameFilter} />
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -57,7 +76,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {personsToShow.map((person) => (
         <Person key={person.name} name={person.name} number={person.number} />
       ))}
     </>
