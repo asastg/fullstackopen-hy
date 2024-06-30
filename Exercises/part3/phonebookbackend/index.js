09a9dda93ecd22013314a9b5d65eef618dce95c8
asastg <asastoqueg@unal.edu.co>
2024-06-30 16:07:58 -0500

    exercise part3.8

--- Exercises/part3/phonebookbackend/index.js
+++ Exercises/part3/phonebookbackend/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-app.use(morgan('tiny'));
 app.use(express.json());
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+    skip: (req, res) => req.method !== 'POST',
+  })
+);
+
+app.use(
+  morgan('tiny', {
+    skip: (req, res) => req.method === 'POST',
+  })
+);
 let persons = [
   {
     id: 1,
