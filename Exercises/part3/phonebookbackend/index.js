0d18b083a99fcb72f18c6cddf10894fd74ebbcc8
asastg <asastoqueg@unal.edu.co>
2024-07-02 07:34:16 -0500

    exercise part3.13

--- Exercises/part3/phonebookbackend/index.js
+++ Exercises/part3/phonebookbackend/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
+const Person = require('./models/person');
 app.use(express.static('dist'));
 app.use(cors());
 app.use(express.json());
@@ -18,46 +20,28 @@ app.use(
     skip: (req, res) => req.method === 'POST',
   })
 );
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-const generateId = () => Math.floor(Math.random() * 99999);
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${Date()}</p>`);
+  response.send(`<p>Phonebook has info for ${Person.length} people</p> <p>${Date()}</p>`);
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
+  // const id = Number(request.params.id);
+  // const person = persons.find((person) => person.id === id);
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
 });
 
 app.post('/api/persons', (request, response) => {
@@ -73,7 +57,6 @@ app.post('/api/persons', (request, response) => {
     });
   }
   const person = {
-    id: generateId(),
     name: body.name,
     number: body.number,
   };
