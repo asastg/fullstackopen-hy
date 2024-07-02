1eb34337325dd4dba9f4fb5e35d10a2e7031b0c0
asastg <asastoqueg@unal.edu.co>
2024-07-02 10:27:33 -0500

    exercise part3.16

--- Exercises/part3/phonebookbackend/index.js
+++ Exercises/part3/phonebookbackend/index.js
@@ -32,10 +32,12 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${Person.length} people</p> <p>${Date()}</p>`);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
   // const id = Number(request.params.id);
   // const person = persons.find((person) => person.id === id);
   // if (person) {
@@ -66,11 +68,28 @@ app.post('/api/persons', (request, response) => {
   });
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then((result) => {
-    response.status(204).end();
-  });
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
