65ef5fc17392108e38dcffb90a5366b7ec67fe44
asastg <asastoqueg@unal.edu.co>
2024-07-04 13:16:33 -0500

    exercise part4.5

--- Exercises/part4/bloglist/utils/list_helper.js
+++ Exercises/part4/bloglist/utils/list_helper.js
@@ -2,7 +2,24 @@ const dummy = () => {
   return 1
 }
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
+
+const favoriteBlog = (blogs) => {
+  let like = 0
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > like) {
+      like = blogs[i].likes
+    }
+  }
+  const blog = blogs.find((blog) => blog.likes === like)
+  const newBlog = {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes,
+  }
+  return newBlog
+}
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
